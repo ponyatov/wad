152c054e876e5a6c2900ae48c8ad43514abfdc05
Dmitry Ponyatov <dponyatov@samsa.local>
2023-12-16 19:13:32 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@samsa.local>

--- kicad/src/kicad.d
+++ kicad/src/kicad.d
@@ -12,10 +12,16 @@ import pegged.grammar;
 
 mixin(grammar(`
     pcb:
-        kicad_pcb <  :l :'kicad_pcb' verzion generator :r | (num|id|l|r|any)*
+        kicad_pcb <  :l :'kicad_pcb' verzion generator general paper layers
+        layers    <  :l :'layers' deflayer* :r
+        deflayer  <  :l num layername ('signal'|'user') layername? :r
+        layername <~ :'\"' [a-zA-Z]+.[a-zA-Z]+ :'\"'
+        paper     <  :l :'paper' :'\"' 'A4' :'\"' :r
+        general   <  :l :'general' thickness :r
+        thickness <  :l :'thickness' num :r
         generator <  :l :'generator' 'pcbnew' :r
         verzion   <  :l :'version' num :r
-        num       <~ [0-9]+
+        num       <~ [0-9]+(.[0-9]+)?
         id        <~ [a-z_][a-z_]*
         l         <  '('
         r         <  ')'
